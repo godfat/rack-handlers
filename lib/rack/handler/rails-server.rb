502ea926207c565434999e0f8f8c81be02a89fb8
Lin Jen-Shin <godfat@godfat.org>
2013-08-22 20:08:05 +0800

    rails-server.rb: add webrick to the list

--- lib/rack/handler/rails-server.rb
+++ lib/rack/handler/rails-server.rb
@@ -1,6 +1,6 @@
 
 if ARGV[0] && ARGV[0] == 'server'[0, ARGV[0].size] && ARGV[1] !~ /^\w+/
-  server = %w[zbatery rainbows unicorn puma thin].find do |s|
+  server = %w[zbatery rainbows unicorn puma thin webrick].find do |s|
     begin
       require s
       true
