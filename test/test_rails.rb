5f71fccb01b10234a5c602de2f04843a3a79cd02
Lin Jen-Shin <godfat@godfat.org>
2016-06-16 16:34:25 +0800

    testing new -C config

--- test/test_rails.rb
+++ test/test_rails.rb
@@ -4,7 +4,9 @@ require_relative 'shared'
 Pork::API.describe 'rails-server' do
   def run name, port, &block
     if get(name)
-      pid = Process.spawn('rails', 's', name, '-p', port.to_s)
+      pid = Process.spawn('rails', 's', name,
+                                   '-p', port.to_s,
+                                   '-C', 'config/environment.rb')
       sleep(1) # slow rails
       block.call(name)
       pid
