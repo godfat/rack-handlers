b003670b468d458d6abcedbd4d69e484baa38de6
Lin Jen-Shin <godfat@godfat.org>
2016-06-16 16:34:35 +0800

    since loading some codes could be messing around...

--- test/test_rails.rb
+++ test/test_rails.rb
@@ -2,8 +2,22 @@
 require_relative 'shared'
 
 Pork::API.describe 'rails-server' do
+  def detect name
+    rd, wr = IO.pipe
+    pid = fork do
+      wr.puts(get(name))
+      wr.close
+    end
+
+    result = rd.gets != "\n"
+    rd.close
+    Process.waitpid(pid)
+
+    result
+  end
+
   def run name, port, &block
-    if get(name)
+    if detect(name)
       pid = Process.spawn('rails', 's', name,
                                    '-p', port.to_s,
                                    '-C', 'config/environment.rb')
